d2930c0dd0fb96cb4b8f1398a2809287e500322a
Brandon <50763955+btror@users.noreply.github.com>
2020-08-07 20:13:32 -0400

    Update Main.java
    Added a menu to start and stop the thread.

--- Main.java
+++ Main.java
@@ -3,10 +3,14 @@ package ColorBoard;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
-import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 /**
@@ -17,12 +21,23 @@ public class Main {
 
     private static JFrame frame;
     private static JPanel[] list = new JPanel[100];
+    private static boolean end_animation = false;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         frame = new JFrame();
+        
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("Menu");
+        JMenuItem start = new JMenuItem("start");
+        JMenuItem stop = new JMenuItem("stop");
+        menu.add(start);
+        menu.add(stop);
+        menuBar.add(menu);
+        frame.setJMenuBar(menuBar);
+        
         frame.setSize(new Dimension(500, 500));
         frame.setTitle("DISCO GUI");
         frame.setResizable(false);
@@ -34,14 +49,28 @@ public class Main {
 
         defaultSetup();
 
-        runAnimation();
+        start.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                end_animation = false;
+                runAnimation();
+            }
+        });
+        
+        stop.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                end_animation = true;
+            }
+        });
+        
 
     }
 
     public static void defaultSetup() {
         for (int i = 0; i < 100; i++) {
             JPanel panel = new JPanel();
-            panel.setBackground(generateRandomColor());
+            panel.setBackground(Color.WHITE);
             frame.add(panel);
             list[i] = panel;
         }
@@ -107,6 +136,9 @@ public class Main {
                     Thread.sleep(5);
                 } catch (InterruptedException ex) {
                     Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                if (end_animation == true){
+                    break;
                 }
             }
 
